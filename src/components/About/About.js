4d13bf8ffdf9f7c9498fc6f7e7f1a024e4b77ba8
Hossain1985 <shikhor31@me.com>
2022-10-02 21:20:10 +0100

    explore Route

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const About = () =>{
+    return(
+        <div>
+            <Header></Header>
+            <h3>This is Real about</h3>
+        </div>
+    )
+
+}
+
+export default About;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const Home = () =>{
+    return(
+        <div>
+            <Header></Header>
+            <h3>
+                This is Real Home
+            </h3>
+        </div>
+    )
+
+}
+
+export default Home;
